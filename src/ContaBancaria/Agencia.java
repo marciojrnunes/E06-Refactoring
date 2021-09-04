92ceeb772d270243a7930d55be5787cf2b123a13
nataliaellem <nataliaem212@gmail.com>
2021-09-03 22:49:07 -0300

    Criação das classes 'Cliente' e 'Agencia'

---
+++ src/ContaBancaria/Agencia.java
@@ -0,0 +1,27 @@
+package ContaBancaria;
+
+public class Agencia {
+
+    private int numAgencia;
+    private String gerente;
+
+
+    public Agencia(int numAgencia, String gerente){
+        this.numAgencia = numAgencia;
+        this.gerente = gerente;
+    }
+    
+    public int getNumAgencia(){
+        return this.numAgencia;
+    }
+
+    public String getGerente(){
+        return this.gerente;
+    }
+
+    public String toString(){
+        String dadosAgencia = String.format("Ag.: %d\nGerente: %s\n",
+        this.numAgencia, this.gerente);
+        return dadosAgencia;
+    }
+}
--- src/ContaBancaria/Cliente.java
+++ src/ContaBancaria/Cliente.java
@@ -1,5 +1,30 @@
 package ContaBancaria;
 
 public class Cliente {
-    
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+
+    public Cliente(String nome, String cpf, String telefone){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    public String getNome(){
+        return this.nome;           
+    }
+    public String getCpf(){
+        return this.cpf;       
+    }
+    public String getTelefone(){
+        return this.telefone;    
+    }
+
+    public String toString(){
+        String dadosCliente = String.format("CPF: %s\nNome: %s\nTelefone: %s",
+        this.cpf, this.nome, this.telefone);
+        return dadosCliente;
+    }
 }
