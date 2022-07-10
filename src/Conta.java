e4529ab55d822340b7f607147d3137f7ee98abcf
Gabriel Soares <brizaelvs@gmail.com>
2022-07-09 22:57:40 -0300

    E03 - Gabriel

--- src/Conta.java
+++ src/Conta.java
@@ -3,71 +3,63 @@ import java.util.List;
 
 public class Conta {
 
-    // TODO(#1) REFATORAR: Esses dados deveriam ficar em outro lugar
-    private String nomeCliente;
-    private String cpfCliente;
-    private String telefoneCliente;
+    private Cliente cliente;
 
-    // TODO(#1) REFATORAR: Esses dados deveriam ficar em outro lugar
-    private int numAgencia;
-    private int numConta;
-    private String gerente;
+    private DB db;
 
-    // TODO(#2) REFATORAR: Esse nome não é o ideal para representar o saldo da conta
-    private double valor;
+    private double saldoAtual;
 
     private List<Operacao> operacoes;
 
-    public Conta(String nomeCliente, String cpfCliente, String telefoneCliente, int numAgencia, int numConta, String gerente, double valor) {
-        this.nomeCliente = nomeCliente;
-        this.cpfCliente = cpfCliente;
-        this.telefoneCliente = telefoneCliente;
-        this.numAgencia = numAgencia;
-        this.numConta = numConta;
-        this.gerente = gerente;
-        this.valor = valor;
+    public Conta(Cliente cliente, DB db, double valor) {
+        this.cliente = cliente;
+        this.db = db;
+        this.saldoAtual = valor;
 
         this.operacoes = new ArrayList<>();
     }
 
     public Conta() {
-        this(null, null, null, 0, 0, null, 0);
+        this(null, null, 0);
     }
 
-    // TODO(#3) REFATORAR: Muita responsabilidade para o mesmo método
     public void realizarOperacao(char tipo, int valor) {
         Operacao op = new Operacao(tipo, valor);
         this.operacoes.add(op);
 
         if (tipo == 'd')
-            this.valor += valor;
+            realizarDeposito(valor);
         else if(tipo == 's')
-            this.valor -= valor;
+            realizarSaque(valor);
+    }
+
+    public void realizarDeposito(int valor){
+        this.saldoAtual += valor;
+    }
+
+    public void realizarSaque(int valor){
+        if(saldoAtual >= valor)
+            this.saldoAtual -= valor;
     }
 
     public String toString() {
-        // TODO(#4) REFATORAR: Esses dados não estão relacionados a conta
-        String dadosCliente = String.format("CPF: %s\nNome: %s\nTelefone: %s",
-                this.cpfCliente, this.nomeCliente, this.telefoneCliente);
+        return "-----CLIENTE-----\n" +
+                cliente.toString() +
+                "\n\n" +
+                "-----CONTA-----\n" +
+                db.toString() +
+                "Saldo: " +  this.saldoAtual + "\n" +
+                "\n\n" +
+                "-----EXTRATO-----\n" +
+                operacoesToString() +
+                "\n";
+    }
 
-        // TODO(#4) REFATORAR: Esses dados não estão relacinados a conta
-        String dadosConta = String.format("Ag.: %d\nConta: %d\nGerente: %s\nSaldo: %.2f",
-                this.numAgencia, this.numConta, this.gerente, this.valor);
-
-        // TODO(#5) REFATORAR: Essa operação não deveria estar sendo realizada neste método
+    public String operacoesToString(){
         String dadosExtrato = "";
         for(Operacao op : this.operacoes) {
             dadosExtrato += op.toString() + "\n";
         }
-
-        return "-----CLIENTE-----\n" +
-                dadosCliente +
-                "\n\n" +
-                "-----CONTA-----\n" +
-                dadosConta +
-                "\n\n" +
-                "-----EXTRATO-----\n" +
-                dadosExtrato +
-                "\n";
+        return dadosExtrato;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Conta minhaConta = new Conta("João", "111.111.111-11", "31 1111-1111",
-                            111, 222222, "Gil", 0.00);
+        Cliente cliente = new Cliente("Gabriel", "151.142.954-21", "31 8561-4513");
+        DB db = new DB(351, 547154, "Teste");
+        Conta minhaConta = new Conta(cliente, db, 0.00);
 
-        minhaConta.realizarOperacao('d', 450);
-        minhaConta.realizarOperacao('s', 50);
-        minhaConta.realizarOperacao('s', 50);
-        minhaConta.realizarOperacao('s', 50);
+        minhaConta.realizarOperacao('d', 85412);
+        minhaConta.realizarOperacao('s', 20000);
+        minhaConta.realizarOperacao('s', 854);
+        minhaConta.realizarOperacao('s', 785);
 
         System.out.println(minhaConta);
     }
