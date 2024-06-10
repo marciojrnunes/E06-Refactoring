00982e1ce0c14cbbd2404a025cc29cbba16919fe
KarenGiullia <138712215+KarenGiullia@users.noreply.github.com>
2024-06-10 15:10:31 -0300

    Entrega

--- src/Operacao.java
+++ src/Operacao.java
@@ -1,5 +1,6 @@
-public class Operacao {
+import java.util.List;
 
+public class Operacao {
     private char tipo;
     private double valor;
 
@@ -8,7 +9,6 @@ public class Operacao {
         this.valor = valor;
     }
 
-    // TODO(#6) REFATORAR: Muita responsabilidade para mesma classe
     public String getTipo() {
         switch (this.tipo) {
             case 'd':
@@ -20,7 +20,29 @@ public class Operacao {
         }
     }
 
+    public static void depositar(Conta conta, double valor) {
+        conta.setSaldo(conta.getSaldo() + valor);
+    }
+
+    public static void sacar(Conta conta, double valor) {
+        if (conta.getSaldo() >= valor) {
+            conta.setSaldo(conta.getSaldo() - valor);
+        } else {
+            System.out.println("Saldo insuficiente para saque");
+        }
+    }
+
+    public static String gerarExtrato(List<Operacao> operacoes) {
+        StringBuilder extrato = new StringBuilder();
+        for (Operacao op : operacoes) {
+            extrato.append(op.toString()).append("\n");
+        }
+        return extrato.toString();
+    }
+
+    @Override
     public String toString() {
         return this.getTipo() + ":\t" + this.valor;
     }
 }
+
